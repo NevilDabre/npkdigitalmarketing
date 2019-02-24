51117e71eeb3ff4cdf15960e3b80c979e6677854
DESKTOP-MGAC9P3\Nevil <ndabre@myeffect.ca>
2019-02-24 14:48:21 -0500

    Email js work complete

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,70 +1,136 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+require('dotenv').config();
 
 const divStyle = {
-  'marginTop': '10px'
+  marginTop: '10px'
 };
 
 export default class ContactForm extends Component {
-  state = {
-    fullName: '',
-    formSubmitted: false
-  };
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+  }
+
+  handleValidation(){
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if(!fields["name"]){
+       formIsValid = false;
+       errors["name"] = "Cannot be empty";
+    }
+
+    //Message
+    if(!fields["message"]){
+          formIsValid = false;
+          errors["message"] = "Cannot be empty";
+       }
+
+    //Phone
+    if(!fields["phone"]){
+      formIsValid = false;
+      errors["phone"] = "Cannot be empty";
+   }
+
+    //Email
+    if(!fields["email"]){
+       formIsValid = false;
+       errors["email"] = "Cannot be empty";
+    }
+
+    if(typeof fields["email"] !== "undefined"){
+       let lastAtPos = fields["email"].lastIndexOf('@');
+       let lastDotPos = fields["email"].lastIndexOf('.');
+
+       if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') === -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+          formIsValid = false;
+          errors["email"] = "Email is not valid";
+        }
+   }  
+
+   this.setState({errors: errors});
+   return formIsValid;
+}
 
   handleCancel = this.handleCancel.bind(this);
-  handleChange = this.handleChange.bind(this);
   handleSubmit = this.handleSubmit.bind(this);
+  handleChange = this.handleChange.bind(this);
 
-  static sender = 'sender@example.com';
 
   handleCancel() {
     this.setState({
-      fullName: ''
+      name: '',
+      phone: '',
+      email: '',
+      message: ''
     });
   }
 
-  handleChange(event) {
-    this.setState({
-      fullName: event.target.value
-    });
-  }
+  handleChange(field, event){         
+    let fields = this.state.fields;
+    fields[field] = event.target.value;        
+    this.setState({fields});
+}
 
   handleSubmit(event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    const {
-      REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-      REACT_APP_EMAILJS_TEMPLATEID: template
-    } = this.props.env;
+        const {
+          REACT_APP_EMAILJS_INQUIRY_TEMPLATEID: webInquiryTemplateId,
+          REACT_APP_EMAILJS_REPLY_TEMPLATEID: webInquiryReplyTemplateId,
+          REACT_APP_EMAILJS_SERVICEID: serviceId,
+          REACT_APP_EMAILJS_USERID: userId
+        } = this.props.env;
+        
+        if(this.handleValidation()){
+          let fields = this.state.fields
+      
+          this.sendContactRequest(
+            webInquiryTemplateId,
+            webInquiryReplyTemplateId,
+            serviceId,
+            userId,
+            fields['name'],
+            fields['phone'],
+            fields['email'],
+            fields['message']
+          );
+      
+          this.setState({
+            formSubmitted: true
+          });
 
-    this.sendFeedback(
-      template,
-      this.sender,
-      receiverEmail,
-      this.state.feedback
-    );
-
-    this.setState({
-      formSubmitted: true
-    });
-
-    this.handleReset();
-
+        }
   }
 
-  sendFeedback(templateId, senderEmail, receiverEmail, feedback) {
+  sendContactRequest( webInquiryTemplateId, webInquiryReplyTemplateId, serviceId, userId, name, phone, email, message) {
+
+    /**
+     *  1. Send Email to Webmaster
+     *  2. Send Email to Inquirer  
+     * */ 
+
     window.emailjs
-      .send('mailgun', templateId, {
-        senderEmail,
-        receiverEmail,
-        feedback
-      })
+      .send(serviceId, 	webInquiryTemplateId, {
+        name, phone, email, message
+      }, userId)
       .then(res => {
+        return window.emailjs
+          .send(serviceId, webInquiryReplyTemplateId,{
+            name, email
+          }, userId)
+      })
+      .then(res=>{
         this.setState({
           formEmailSent: true
         });
       })
-      // Handle errors here however you like
       .catch(err => console.error('Failed to send feedback. Error: ', err));
   }
 
@@ -73,21 +139,24 @@ export default class ContactForm extends Component {
       <form className="block mbr-form" id="contactForm" onSubmit={this.handleSubmit}>
         <div className="row">
         <div className="col-6 multi-horizontal">
-        <input name="fullName" type="text" 
-            placeholder="Your Name" className="form-control input" onChange={this.handleChange} value={this.state.fullName}  />
+        <input name="name" type="text" placeholder="Your Name" className="form-control input" onChange={this.handleChange.bind(this, "name")} value={this.state.name}  />
+            <span style={{color: "red"}}>{this.state.errors["name"]}</span>
         </div>
         <div className="col-6 multi-horizontal">
-        <input name="phone" className="form-control input" type="number" placeholder="Phone Number" onChange={this.handleChange} value={this.state.phone} />
+        <input name="phone" className="form-control input" type="number" placeholder="Phone Number" onChange={this.handleChange.bind(this, "phone")} value={this.state.phone} />
+        <span style={{color: "red"}}>{this.state.errors["phone"]}</span>
         </div>
         </div>
         <div className="row">
         <div className="col-12">
-        <input name="email" className="form-control input" type="email" placeholder="Email Address" onChange={this.handleChange} value={this.state.email} />
+        <input name="email" className="form-control input" type="email" placeholder="Email Address" onChange={this.handleChange.bind(this, "email")} value={this.state.email} />
+        <span style={{color: "red"}}>{this.state.errors["email"]}</span>
         </div>
           </div>
         <div className="row">
           <div className="col-12">
-            <textarea name="message" className="form-control" rows="3" onChange={this.handleChange} value={this.state.message}></textarea>
+            <textarea name="message" className="form-control" rows="3" value={this.state.message} onChange={this.handleChange.bind(this, "message")}></textarea>
+            <span style={{color: "red"}}>{this.state.errors["message"]}</span>
           </div>
         </div>
         <div className="col-12 input-group-btn" style={divStyle}>
